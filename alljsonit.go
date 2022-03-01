b8b732fe2778cecea8b31e2f6a9a01b77f926e6c
1gtm <1gtm@appscode.com>
2022-03-01 10:11:12 +0000

    Cancel concurrent CI runs for same pr/commit
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- alljsonit.go
+++ alljsonit.go
@@ -51,266 +51,264 @@ type Data struct {
 	ResourceType string
 }
 
-var (
-	allJsonIt = map[schema.GroupVersionResource]Data{
-		{
-			Group:    "alert.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "configurations",
-		}: {
-			JsonIt:       controllers.GetJSONItr(alertv1alpha1.GetEncoder(), alertv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_alert_configuration",
-		},
-		{
-			Group:    "auditing.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "auditings",
-		}: {
-			JsonIt:       controllers.GetJSONItr(auditingv1alpha1.GetEncoder(), auditingv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_auditing",
-		},
-		{
-			Group:    "cloud.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "provideraccesses",
-		}: {
-			JsonIt:       controllers.GetJSONItr(cloudv1alpha1.GetEncoder(), cloudv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_cloud_provider_access",
-		},
-		{
-			Group:    "cloud.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "provideraccessauthorizations",
-		}: {
-			JsonIt:       controllers.GetJSONItr(cloudv1alpha1.GetEncoder(), cloudv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_cloud_provider_access_authorization",
-		},
-		{
-			Group:    "cloud.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "provideraccesssetups",
-		}: {
-			JsonIt:       controllers.GetJSONItr(cloudv1alpha1.GetEncoder(), cloudv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_cloud_provider_access_setup",
-		},
-		{
-			Group:    "cloud.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "providersnapshots",
-		}: {
-			JsonIt:       controllers.GetJSONItr(cloudv1alpha1.GetEncoder(), cloudv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_cloud_provider_snapshot",
-		},
-		{
-			Group:    "cloud.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "providersnapshotbackuppolicies",
-		}: {
-			JsonIt:       controllers.GetJSONItr(cloudv1alpha1.GetEncoder(), cloudv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_cloud_provider_snapshot_backup_policy",
-		},
-		{
-			Group:    "cloud.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "providersnapshotrestorejobs",
-		}: {
-			JsonIt:       controllers.GetJSONItr(cloudv1alpha1.GetEncoder(), cloudv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_cloud_provider_snapshot_restore_job",
-		},
-		{
-			Group:    "cluster.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "clusters",
-		}: {
-			JsonIt:       controllers.GetJSONItr(clusterv1alpha1.GetEncoder(), clusterv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_cluster",
-		},
-		{
-			Group:    "custom.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "dbroles",
-		}: {
-			JsonIt:       controllers.GetJSONItr(customv1alpha1.GetEncoder(), customv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_custom_db_role",
-		},
-		{
-			Group:    "custom.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "dnsconfigurationclusteraws",
-		}: {
-			JsonIt:       controllers.GetJSONItr(customv1alpha1.GetEncoder(), customv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_custom_dns_configuration_cluster_aws",
-		},
-		{
-			Group:    "data.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "lakes",
-		}: {
-			JsonIt:       controllers.GetJSONItr(datav1alpha1.GetEncoder(), datav1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_data_lake",
-		},
-		{
-			Group:    "database.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "users",
-		}: {
-			JsonIt:       controllers.GetJSONItr(databasev1alpha1.GetEncoder(), databasev1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_database_user",
-		},
-		{
-			Group:    "encryption.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "atrests",
-		}: {
-			JsonIt:       controllers.GetJSONItr(encryptionv1alpha1.GetEncoder(), encryptionv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_encryption_at_rest",
-		},
-		{
-			Group:    "event.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "triggers",
-		}: {
-			JsonIt:       controllers.GetJSONItr(eventv1alpha1.GetEncoder(), eventv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_event_trigger",
-		},
-		{
-			Group:    "global.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "clusterconfigs",
-		}: {
-			JsonIt:       controllers.GetJSONItr(globalv1alpha1.GetEncoder(), globalv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_global_cluster_config",
-		},
-		{
-			Group:    "ldap.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "configurations",
-		}: {
-			JsonIt:       controllers.GetJSONItr(ldapv1alpha1.GetEncoder(), ldapv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_ldap_configuration",
-		},
-		{
-			Group:    "ldap.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "verifies",
-		}: {
-			JsonIt:       controllers.GetJSONItr(ldapv1alpha1.GetEncoder(), ldapv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_ldap_verify",
-		},
-		{
-			Group:    "maintenance.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "windows",
-		}: {
-			JsonIt:       controllers.GetJSONItr(maintenancev1alpha1.GetEncoder(), maintenancev1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_maintenance_window",
-		},
-		{
-			Group:    "network.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "containers",
-		}: {
-			JsonIt:       controllers.GetJSONItr(networkv1alpha1.GetEncoder(), networkv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_network_container",
-		},
-		{
-			Group:    "network.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "peerings",
-		}: {
-			JsonIt:       controllers.GetJSONItr(networkv1alpha1.GetEncoder(), networkv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_network_peering",
-		},
-		{
-			Group:    "online.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "archives",
-		}: {
-			JsonIt:       controllers.GetJSONItr(onlinev1alpha1.GetEncoder(), onlinev1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_online_archive",
-		},
-		{
-			Group:    "private.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "ipmodes",
-		}: {
-			JsonIt:       controllers.GetJSONItr(privatev1alpha1.GetEncoder(), privatev1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_private_ip_mode",
-		},
-		{
-			Group:    "privatelink.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "endpoints",
-		}: {
-			JsonIt:       controllers.GetJSONItr(privatelinkv1alpha1.GetEncoder(), privatelinkv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_privatelink_endpoint",
-		},
-		{
-			Group:    "privatelink.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "endpointservices",
-		}: {
-			JsonIt:       controllers.GetJSONItr(privatelinkv1alpha1.GetEncoder(), privatelinkv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_privatelink_endpoint_service",
-		},
-		{
-			Group:    "project.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "projects",
-		}: {
-			JsonIt:       controllers.GetJSONItr(projectv1alpha1.GetEncoder(), projectv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_project",
-		},
-		{
-			Group:    "project.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "ipaccesslists",
-		}: {
-			JsonIt:       controllers.GetJSONItr(projectv1alpha1.GetEncoder(), projectv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_project_ip_access_list",
-		},
-		{
-			Group:    "search.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "indices",
-		}: {
-			JsonIt:       controllers.GetJSONItr(searchv1alpha1.GetEncoder(), searchv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_search_index",
-		},
-		{
-			Group:    "team.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "teams",
-		}: {
-			JsonIt:       controllers.GetJSONItr(teamv1alpha1.GetEncoder(), teamv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_team",
-		},
-		{
-			Group:    "teams.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "teams",
-		}: {
-			JsonIt:       controllers.GetJSONItr(teamsv1alpha1.GetEncoder(), teamsv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_teams",
-		},
-		{
-			Group:    "third.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "partyintegrations",
-		}: {
-			JsonIt:       controllers.GetJSONItr(thirdv1alpha1.GetEncoder(), thirdv1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_third_party_integration",
-		},
-		{
-			Group:    "x509.mongodbatlas.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "authenticationdatabaseusers",
-		}: {
-			JsonIt:       controllers.GetJSONItr(x509v1alpha1.GetEncoder(), x509v1alpha1.GetDecoder()),
-			ResourceType: "mongodbatlas_x509_authentication_database_user",
-		},
-	}
-)
+var allJsonIt = map[schema.GroupVersionResource]Data{
+	{
+		Group:    "alert.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "configurations",
+	}: {
+		JsonIt:       controllers.GetJSONItr(alertv1alpha1.GetEncoder(), alertv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_alert_configuration",
+	},
+	{
+		Group:    "auditing.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "auditings",
+	}: {
+		JsonIt:       controllers.GetJSONItr(auditingv1alpha1.GetEncoder(), auditingv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_auditing",
+	},
+	{
+		Group:    "cloud.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "provideraccesses",
+	}: {
+		JsonIt:       controllers.GetJSONItr(cloudv1alpha1.GetEncoder(), cloudv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_cloud_provider_access",
+	},
+	{
+		Group:    "cloud.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "provideraccessauthorizations",
+	}: {
+		JsonIt:       controllers.GetJSONItr(cloudv1alpha1.GetEncoder(), cloudv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_cloud_provider_access_authorization",
+	},
+	{
+		Group:    "cloud.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "provideraccesssetups",
+	}: {
+		JsonIt:       controllers.GetJSONItr(cloudv1alpha1.GetEncoder(), cloudv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_cloud_provider_access_setup",
+	},
+	{
+		Group:    "cloud.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "providersnapshots",
+	}: {
+		JsonIt:       controllers.GetJSONItr(cloudv1alpha1.GetEncoder(), cloudv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_cloud_provider_snapshot",
+	},
+	{
+		Group:    "cloud.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "providersnapshotbackuppolicies",
+	}: {
+		JsonIt:       controllers.GetJSONItr(cloudv1alpha1.GetEncoder(), cloudv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_cloud_provider_snapshot_backup_policy",
+	},
+	{
+		Group:    "cloud.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "providersnapshotrestorejobs",
+	}: {
+		JsonIt:       controllers.GetJSONItr(cloudv1alpha1.GetEncoder(), cloudv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_cloud_provider_snapshot_restore_job",
+	},
+	{
+		Group:    "cluster.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "clusters",
+	}: {
+		JsonIt:       controllers.GetJSONItr(clusterv1alpha1.GetEncoder(), clusterv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_cluster",
+	},
+	{
+		Group:    "custom.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "dbroles",
+	}: {
+		JsonIt:       controllers.GetJSONItr(customv1alpha1.GetEncoder(), customv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_custom_db_role",
+	},
+	{
+		Group:    "custom.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "dnsconfigurationclusteraws",
+	}: {
+		JsonIt:       controllers.GetJSONItr(customv1alpha1.GetEncoder(), customv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_custom_dns_configuration_cluster_aws",
+	},
+	{
+		Group:    "data.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "lakes",
+	}: {
+		JsonIt:       controllers.GetJSONItr(datav1alpha1.GetEncoder(), datav1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_data_lake",
+	},
+	{
+		Group:    "database.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "users",
+	}: {
+		JsonIt:       controllers.GetJSONItr(databasev1alpha1.GetEncoder(), databasev1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_database_user",
+	},
+	{
+		Group:    "encryption.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "atrests",
+	}: {
+		JsonIt:       controllers.GetJSONItr(encryptionv1alpha1.GetEncoder(), encryptionv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_encryption_at_rest",
+	},
+	{
+		Group:    "event.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "triggers",
+	}: {
+		JsonIt:       controllers.GetJSONItr(eventv1alpha1.GetEncoder(), eventv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_event_trigger",
+	},
+	{
+		Group:    "global.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "clusterconfigs",
+	}: {
+		JsonIt:       controllers.GetJSONItr(globalv1alpha1.GetEncoder(), globalv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_global_cluster_config",
+	},
+	{
+		Group:    "ldap.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "configurations",
+	}: {
+		JsonIt:       controllers.GetJSONItr(ldapv1alpha1.GetEncoder(), ldapv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_ldap_configuration",
+	},
+	{
+		Group:    "ldap.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "verifies",
+	}: {
+		JsonIt:       controllers.GetJSONItr(ldapv1alpha1.GetEncoder(), ldapv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_ldap_verify",
+	},
+	{
+		Group:    "maintenance.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "windows",
+	}: {
+		JsonIt:       controllers.GetJSONItr(maintenancev1alpha1.GetEncoder(), maintenancev1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_maintenance_window",
+	},
+	{
+		Group:    "network.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "containers",
+	}: {
+		JsonIt:       controllers.GetJSONItr(networkv1alpha1.GetEncoder(), networkv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_network_container",
+	},
+	{
+		Group:    "network.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "peerings",
+	}: {
+		JsonIt:       controllers.GetJSONItr(networkv1alpha1.GetEncoder(), networkv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_network_peering",
+	},
+	{
+		Group:    "online.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "archives",
+	}: {
+		JsonIt:       controllers.GetJSONItr(onlinev1alpha1.GetEncoder(), onlinev1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_online_archive",
+	},
+	{
+		Group:    "private.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "ipmodes",
+	}: {
+		JsonIt:       controllers.GetJSONItr(privatev1alpha1.GetEncoder(), privatev1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_private_ip_mode",
+	},
+	{
+		Group:    "privatelink.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "endpoints",
+	}: {
+		JsonIt:       controllers.GetJSONItr(privatelinkv1alpha1.GetEncoder(), privatelinkv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_privatelink_endpoint",
+	},
+	{
+		Group:    "privatelink.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "endpointservices",
+	}: {
+		JsonIt:       controllers.GetJSONItr(privatelinkv1alpha1.GetEncoder(), privatelinkv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_privatelink_endpoint_service",
+	},
+	{
+		Group:    "project.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "projects",
+	}: {
+		JsonIt:       controllers.GetJSONItr(projectv1alpha1.GetEncoder(), projectv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_project",
+	},
+	{
+		Group:    "project.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "ipaccesslists",
+	}: {
+		JsonIt:       controllers.GetJSONItr(projectv1alpha1.GetEncoder(), projectv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_project_ip_access_list",
+	},
+	{
+		Group:    "search.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "indices",
+	}: {
+		JsonIt:       controllers.GetJSONItr(searchv1alpha1.GetEncoder(), searchv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_search_index",
+	},
+	{
+		Group:    "team.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "teams",
+	}: {
+		JsonIt:       controllers.GetJSONItr(teamv1alpha1.GetEncoder(), teamv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_team",
+	},
+	{
+		Group:    "teams.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "teams",
+	}: {
+		JsonIt:       controllers.GetJSONItr(teamsv1alpha1.GetEncoder(), teamsv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_teams",
+	},
+	{
+		Group:    "third.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "partyintegrations",
+	}: {
+		JsonIt:       controllers.GetJSONItr(thirdv1alpha1.GetEncoder(), thirdv1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_third_party_integration",
+	},
+	{
+		Group:    "x509.mongodbatlas.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "authenticationdatabaseusers",
+	}: {
+		JsonIt:       controllers.GetJSONItr(x509v1alpha1.GetEncoder(), x509v1alpha1.GetDecoder()),
+		ResourceType: "mongodbatlas_x509_authentication_database_user",
+	},
+}
 
 func getJsonItAndResType(gvr schema.GroupVersionResource) Data {
 	return allJsonIt[gvr]
